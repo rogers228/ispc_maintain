33b9405b9dc8e81d021f1adfe29a3309588be2c3
rogers228 <61651257+rogers228@users.noreply.github.com>
2025-10-20 20:27:28 +0800

    新增

---
+++ tempstorage/template.py
@@ -0,0 +1,15 @@
+# 此檔案為範本
+
+# 以下註解 section basic 字符不可刪除，給 re 正則比對使用
+if True: # -----section system start-----
+    system = {
+        'test': 'template',
+        'version': '9-20251020',
+    }
+    # -----section system end-----
+
+def test1():
+    print(system)
+
+if __name__ == '__main__':
+    test1()
