6f62f84a9bc1b2964cf69bf992fbabfd740be3c3
rogers Lu <61651257+rogers228@users.noreply.github.com>
2025-10-21 14:35:09 +0800

    新增

--- tempstorage/template.py
+++ tempstorage/template.py
@@ -1,16 +1,22 @@
 # 此檔案為範本
 
 # 以下註解 section system 字符不可刪除，給 re 正則比對使用
+# 儲存成功後會回寫 system，與雲端相同資料
+# 故 system dict 就代表上一次上傳的資訊
+# 用來判斷
+# 使用者不要編輯
+
 if True: # -----section system start-----
     system = {
-        'id': 'test',
         'data_hash': 'test',
         'last_time': 'test',
         'edit_user': 'test',
-        'version': '10-20251021',
     }
     # -----section system end-----
 
+specification = {
+
+}
 
 def test1():
     print(system)
